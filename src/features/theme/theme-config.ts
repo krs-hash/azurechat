749555566feac60a37d68a2b0fce131c34d1968b
krs-hash <kyle.schlieve@wheda.com>
2024-10-17 08:55:34 -0500

    Update theme-config.ts

--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -1,5 +1,5 @@
-export const AI_NAME = "Azure Chat";
-export const AI_DESCRIPTION = "Azure Chat is a friendly AI assistant.";
+export const AI_NAME = "WHEDA Chat";
+export const AI_DESCRIPTION = "WHEDA Chat is a friendly AI assistant. Im pretty useless for current events since the last time I learned anything was in October 2023. I can learn more, but it will require money to send me to training!";
 export const CHAT_DEFAULT_PERSONA = AI_NAME + " default";
 
 export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant. You must always return in markdown format.
